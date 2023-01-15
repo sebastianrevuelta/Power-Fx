af93e88da7d4f149e44d74a165e8df37567f77ef
Luc Genetier <lucgen@microsoft.com>
2023-01-15 21:22:38 +0100

    Add ValidateDotNetVersion test

---
+++ src/tests/Microsoft.PowerFx.Core.Tests/NetVersionValidationTests.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+using Xunit;
+
+namespace Microsoft.PowerFx.Core.Tests
+{
+    public class NetVersionValidationTests
+    {
+        [Fact]
+        public void ValidateDotNetVersion()
+        {
+            string netVersion = typeof(string).Assembly.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName
+                    ?? AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName;
+            netVersion = netVersion.Substring(netVersion.Length - 4);
+
+#if NETCOREAPP3_1
+            Assert.Equal("v2.1", netVersion);
+#elif NET6_0 
+            Assert.Equal("v6.0", netVersion);
+#elif NET7_0
+            Assert.Equal("v7.0", netVersion);
+#else
+#error Invalid .Net version    
+#endif
+        }
+    }
+}
