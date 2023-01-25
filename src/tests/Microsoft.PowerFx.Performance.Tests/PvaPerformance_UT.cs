d0ebc57d553821aedac942abdc82e05902b9e2c7
Luc Genetier <lucgen@microsoft.com>
2023-01-25 13:39:39 +0100

    Add UT

---
+++ src/tests/Microsoft.PowerFx.Performance.Tests/PvaPerformance_UT.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Microsoft.PowerFx.Performance.Tests
+{
+    public class PvaPerformance_UT 
+    {
+        protected PvaPerformance pp;
+
+        public PvaPerformance_UT()             
+        {
+            pp = new PvaPerformance();
+            pp.GlobalSetup();
+        }
+
+        [Fact]
+        public void PvaPerformance_PvaRecalcEngineConstructorWith10KOptionSets()
+        {            
+            RecalcEngine engine = pp.PvaRecalcEngineConstructorWith10KOptionSets();
+            Assert.NotNull(engine);
+        }
+    }
+}
